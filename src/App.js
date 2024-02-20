acb2102444eecf8ce0053018e55aebba3073e1bc
Matheus Lucas <matheus.lucas1105@gmail.com>
2024-02-20 16:07:05 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TimelineComponent from './Components/TimelineComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <TimelineComponent />
+    </div>
+  );
+}
+
+export default App;
